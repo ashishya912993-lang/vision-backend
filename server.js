8979853cb1912dccc8276db93971dd1db4e21a3f
ashishya912993-lang <ashishya912993@gmail.com>
2026-04-01 14:46:48 +0530

    Add CORS support to the Express server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from "express";
+const cors = require("cors");
 import OpenAI from "openai";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const client = new OpenAI({
